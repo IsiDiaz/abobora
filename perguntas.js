c9ee178311f723cf05f84872d24c10118ffd1f95
IsiDiaz <142444818+IsiDiaz@users.noreply.github.com>
2024-10-07 10:25:02 -0300

    fdfdfdf

---
+++ perguntas.js
@@ -0,0 +1,100 @@
+criaCartao (
+    'Geografia', 
+    'O que é latitude e longitude, e qual a sua importância na localização geográfica?',
+    ' Latitude é a distância, medida em graus, entre um ponto qualquer da superfície terrestre e a linha do Equador. Longitude é a distância, também medida em graus, entre um ponto qualquer da superfície terrestre e o meridiano de Greenwich. Juntas, latitude e longitude são usadas para determinar a localização exata de qualquer ponto na superfície terrestre, formando um sistema de coordenadas geográficas.')
+    
+    criaCartao (
+    'Geografia',
+    'O que é a tectônica de placas e como ela influencia a formação de montanhas e vulcões?',
+    'A tectônica de placas é a teoria que explica o movimento das grandes placas que compõem a crosta terrestre. Esse movimento é responsável por processos como a formação de montanhas (quando duas placas colidem) e de vulcões (quando uma placa é empurrada para baixo de outra, derretendo e formando magma, que pode subir à superfície). ' )
+    
+    criaCartao (
+    'Geografia ',
+    'O que são biomas e qual a principal característica do bioma Amazônia? ',
+    ' Biomas são grandes regiões com características climáticas, vegetativas e faunísticas semelhantes. O bioma Amazônia é caracterizado por sua grande biodiversidade, clima quente e úmido, e pela presença da maior floresta tropical do mundo.')
+    
+    criaCartao (
+    'Geografia ',
+    'Quais são os principais impactos ambientais causados pelo desmatamento? ',
+    ' O desmatamento causa perda de biodiversidade, erosão do solo, alteração do ciclo da água, aumento das emissões de gases de efeito estufa (como o CO₂), e pode contribuir para mudanças climáticas, além de afetar comunidades locais que dependem da floresta.')
+    
+    criaCartao (
+    'Geografia',
+    'Qual a diferença entre tempo e clima?',
+    'O tempo refere-se às condições atmosféricas momentâneas de uma região, como temperatura, umidade e precipitação, que podem variar diariamente. Já o clima é a média dessas condições ao longo de longos períodos (geralmente 30 anos ou mais) em uma determinada região. ')
+    
+    criaCartao (
+    'Biologia',
+    'O que é uma célula? ',
+    'A célula é a unidade fundamental da vida. Ela é a menor estrutura capaz de realizar todas as atividades necessárias para a vida, como metabolismo, crescimento e reprodução. Existem dois tipos principais: células procariontes (sem núcleo definido, como as bactérias) e células eucariontes (com núcleo definido, como nas plantas e animais). ')
+    
+    criaCartao (
+    'Biologia ',
+    'Qual é a função da mitocôndria? ',
+    'A mitocôndria é a organela responsável pela produção de energia nas células eucariontes. Ela converte nutrientes, como a glicose, em ATP (adenosina trifosfato), a principal fonte de energia para as atividades celulares. ')
+    
+    criaCartao (
+    'Biologia',
+    'O que é fotossíntese? ',
+    'A fotossíntese é o processo pelo qual plantas, algas e algumas bactérias utilizam a energia da luz solar para converter dióxido de carbono (CO2) e água (H2O) em glicose (C6H12O6) e oxigênio (O2). Esse processo é fundamental para a produção de energia nas plantas e para a liberação de oxigênio na atmosfera. ')
+    
+    criaCartao (
+    'Biologia ',
+    'Qual é a diferença entre DNA e RNA? ',
+    'O DNA (ácido desoxirribonucleico) é a molécula que carrega o código genético de todos os organismos vivos, armazenando informações necessárias para o desenvolvimento e funcionamento de um organismo. O RNA (ácido ribonucleico) atua na síntese de proteínas, copiando e transportando as informações do DNA para a produção de proteínas. Uma diferença fundamental é que o DNA tem a base nitrogenada timina (T), enquanto o RNA possui uracila (U) em seu lugar. ')
+    
+    criaCartao (
+    'Biologia ',
+    'O que é seleção natural? ',
+    'A seleção natural é o processo pelo qual organismos que possuem características vantajosas para a sobrevivência em um determinado ambiente tendem a sobreviver e se reproduzir com mais sucesso do que aqueles que não possuem essas características. Com o tempo, essas características vantajosas se tornam mais comuns em uma população. Esse conceito é central para a teoria da evolução proposta por Charles Darwin. ')
+    
+    criaCartao (
+    'Historia',
+    'Quem eram os dois grupos da Revolução Francesa? ',
+    'Os girondinos e os jacobinos.')
+    
+    criaCartao (
+    'Historia ',
+    'Qual foi o principal evento que iniciou a Revolução Francesa?',
+    'A queda da Bastilha. ')
+    
+    criaCartao (
+    'Historia',
+    'Quais foram os dois exércitos da Revolução Russa?',
+    'O exército vermelho e o exército branco.')
+    
+    criaCartao (
+    'Historia',
+    'O que ocorreu com a família Romanov com os ocorridos da Revolução Russa?',
+    'Foram todos mortos.')
+    
+    criaCartao (
+    'Historia',
+    'Após a Revolução Russa, quem foi o novo líder político da União Soviética?',
+    'Ulianov Lenin.'
+)
+    
+    criaCartao (
+    'Portugues',
+    'Qual foi a maior obra de Machado de Assis que iniciou o movimento Realista no Brasil?',
+    'Memorias Póstumas de Brás Cubas.')
+    
+    criaCartao (
+    'Portugues',
+    'O que o livro Memórias Póstumas De Brás Cubas inovou na literatura?',
+    'Trouxe o defunto autor.')
+    
+    criaCartao (
+    'Portugues',
+    'Quais as principais características de Machado de Assis?',
+    'A antilineariedade, a ironia e o realismo')
+    
+    criaCartao (
+    'Portugues',
+    'Qual foi a obra que inaugurou o Naturalismo no Brasil?',
+    'O Mulato, de Aluísio Azevedo')
+    
+    criaCartao (
+    'Portugues',
+    'Qual a característica principal do Naturalismo?',
+    'A interpretação do homem como um ser instintivo e produto do ambiente.')
