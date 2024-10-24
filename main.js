ec9ace476693abd9fce61c1c69a54ef1c88b9390
IsiDiaz <142444818+IsiDiaz@users.noreply.github.com>
2024-10-24 08:16:42 -0300

    deddeded

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ let container = document.getElementById('container');
 let cartao = document.createElement('article');
 cartao.className = 'cartao';
 cartao.innerHTML= ´<div class="conteudo-cartao">
-<h3>categoria</h3>
+<h3>${categoria}</h3>
 <div class="pergunta-cartao">
     <p> pergunta </p>
 </div>
