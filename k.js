9eb56c7ef41ab9382c417193d22e615404400c22
IsiDiaz <142444818+IsiDiaz@users.noreply.github.com>
2024-10-31 08:33:32 -0300

    bf

--- k.js
+++ k.js
@@ -1,30 +1,17 @@
-:root{
-    --fundo: #8eacac;
-    --cor-frente: #00BFFF	;
-    --cor-verso: #87CEFA	;
-    --destaque: #7FFFD4	;
-    --preto: #00000000;
-    --branco: #ffffff;
-}
-body {
-    background-color: var(--fundo);
-}
-
-
-header {
-    background-color: var(--preto);
-    padding: 40px;
-    display: flex;
-    justify-content: space-around;
-}
-header a {
-    color: var(--branco);
-}
-footer {
-    position: fixed;
-    bottom: 0;
-    background-color: var(--preto);
-    color: var(--branco);
-    width: 100%;
-    text-align: center;
-}
+function criaCartao (categoria,pergunta,resposta){
+    let container = document.getElementById('container');
+    let cartao = document.createElement('article');
+    cartao.className = 'cartao';
+    cartao.innerHTML= `<div class="conteudo-cartao">
+    <h3>${categoria}</h3>
+    <div class="pergunta-cartao">
+        <p> ${pergunta} </p>
+    </div>
+    <div class="reposta-cartao">
+        <p> ${resposta} </p>
+    </div>
+    </div>`
+    
+    container.appendChild(cartao);
+    }
+    
