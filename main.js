37e4b410c8e16d16b36cc6992260c9ea9d45f182
IsiDiaz <142444818+IsiDiaz@users.noreply.github.com>
2024-10-31 08:50:19 -0300

    ddd

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ cartao.innerHTML= `<div class="conteudo-cartao">
 <div class="pergunta-cartao">
     <p> ${pergunta} </p>
 </div>
-<div class="reposta-cartao">
+<div class="resposta-cartao">
     <p> ${resposta} </p>
 </div>
 </div>`
